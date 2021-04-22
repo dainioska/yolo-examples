7ef07c41b2d4ab8d95ba66e3980925dd63049ab3
dainioshka <dainioshka@gmail.com>
2021-04-22 12:38:15 +0300

    add dir

---
+++ YoloTrainer/yolo_trainer.py
@@ -0,0 +1,17 @@
+# Angle TRAINER files from murtaza
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture("Samples/arm_01.mp4")
+
+while True:
+    _, img = cap.read()
+    img = cv2.resize(img, (640, 480))
+    #imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    cv2.imshow("Image",img)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
