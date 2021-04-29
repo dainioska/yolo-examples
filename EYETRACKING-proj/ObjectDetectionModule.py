41b3bb70ef2a60658284810aa2ec975f604d838a
dainioshka <dainioshka@gmail.com>
2021-04-29 09:20:27 +0300

    edit files

--- EYETRACKING-proj/ObjectDetectionModule.py
+++ EYETRACKING-proj/ObjectDetectionModule.py
@@ -3,6 +3,9 @@ Object detection module MODIFIED
 """
 import cv2
 
+path1 = ('/home/rodney/Desktop/GIT_py/yolo-examples/Yolo-Testukas/Resources/lena.png')
+path2 = ('/home/rodney/Desktop/GIT_py/yolo-examples/Yolo-Testukas/Resources/haarcascade_frontalface_default.xml')
+
 def findObjects(img, objectCascade, scaleF=1.1, minN =4):
     """
        finds objects using haarcascade file
@@ -12,19 +15,21 @@ def findObjects(img, objectCascade, scaleF=1.1, minN =4):
        :param minN: how many neighbours each rectangle should have to accept as valid
        :return: image with rectangles draw and the bounding bow info
     """
-    imgObjects =img.copy()
+    imgObjects = img.copy()
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     objects = objectCascade.detectMultiScale(imgGray, scaleF, minN)
     objectsOut = []
     for (x, y, w, h) in objects:
         cv2.rectangle(imgObjects, (x, y), (x+w, y+h), (255, 0, 255), 2)
-        objectsOut.append([[x, y, w, h],[w+h])
+        objectsOut.append([[x, y, w, h],[w+h]])
 
-    return imgObjects, objects
+    objectsOut = sorted(objectsOut, key=lambda x:x[1], reverse=True)
+
+    return imgObjects, objectsOut
 
 def main():
-    img = cv2.imread('./Resources/lena.png')
-    faceCascade = cv2.CascadeClassifier("./Resources/haarcascade_frontalface_default.xml")
+    img = cv2.imread(path1)
+    faceCascade = cv2.CascadeClassifier(path2)
     imgObjects, objects = findObjects(img, faceCascade)
     cv2.imshow("Output", imgObjects)
     cv2.waitKey(0)
