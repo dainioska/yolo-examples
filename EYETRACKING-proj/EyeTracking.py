9d02c23617f98780796cb008a2684ffc2d407cca
dainioshka <dainioshka@gmail.com>
2021-04-30 20:36:33 +0300

    edit dir

--- EYETRACKING-proj/EyeTracking.py
+++ EYETRACKING-proj/EyeTracking.py
@@ -2,13 +2,15 @@ import cv2
 import numpy as np
 import SerialModule as sm
 import ObjectDetectionModule as odm
+
 import time
 
 frameW = 640
 frameH = 480
 path1 ='/home/rodney/Desktop/GIT_py/yolo-examples/EYETRACKING-proj/haarcascade_frontalface_default.xml'
+perrorLR, perrorUD = 0,0
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 faceCascade = cv2.CascadeClassifier(path1)
 
 def findCenter(imgObjects, objects):
@@ -24,6 +26,28 @@ def findCenter(imgObjects, objects):
         cv2.line(imgObjects, (cx, ih//2), (cx, cy), (0, 255, 0), 1)
     return cx, cy, imgObjects
 
+def trackObject(cx, cy, w, h):
+    global perrorLR,perrorUD
+
+    kLR = [0.6, 0.1]
+    kUD = [0.6, 0.1]
+
+    if cx != -1: 
+        # Left and Rigth
+        errorLR = w//2 - cx
+        posX = kLR[0] * errorLR + kUD[1] * (errorLR - perrorLR)
+        posx = int(np.integer(posX, [-w//2, w//2], [20, 160]))
+        perrorLR = errorLR
+
+        # Up and Down
+        errorUD = h//2 - cy
+        posY = kUD[0] * errorUD + kUD[1] * (errorUD - perrorUD)
+        posY = int(np.integer(posY, [-h//2, h//2], [20, 160]))
+        perrorUD = errorUD
+
+        sm.sendData(ser, [posX, posY], 3)
+
+
 
 while True:
     _, img = cap.read()
@@ -34,6 +58,8 @@ while True:
     h, w, c = imgObjects.shape
     cv2.line(imgObjects, (w//2, 0), (w//2, h), (255, 0, 255), 1)
     cv2.line(imgObjects, (0, h//2), (w, h//2), (255, 0, 255), 1)
+
+    trackObject(cx, cy, w, h)
     
     img = cv2.resize(imgObjects, (0,0), None, 3, 3)
     cv2.imshow("Image",img)
