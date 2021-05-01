871f558d57eb7f1b7c8f3cc27304928364f22c8b
dainioshka <dainioshka@gmail.com>
2021-05-01 10:22:54 +0300

    edit dir

--- FaceDetection-project/FaceDetectionBasics.py
+++ FaceDetection-project/FaceDetectionBasics.py
@@ -6,9 +6,16 @@ import mediapipe as mp
 import  time
 
 cap =cv2.VideoCapture("Video/arm_01.mp4")
+pTime = 0
 
 while True:
     _, img = cap.read()
+
+    # ---show FPS on image
+    cTime =time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
 
 
     cv2.imshow("Image",img)
