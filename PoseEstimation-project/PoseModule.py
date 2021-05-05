25a150546c932e1b90543ab26f1679015ac29184
dainioshka <dainioshka@gmail.com>
2021-05-05 14:01:55 +0300

    add pose module

--- PoseEstimation-project/PoseModule.py
+++ PoseEstimation-project/PoseModule.py
@@ -6,39 +6,64 @@ import cv2
 import mediapipe as mp
 import time
 
-mpDraw = mp.solutions.drawing_utils
-mpPose = mp.solutions.pose
-pose = mpPose.Pose()
+class poseDetector():
 
-# cap = cv2.VideoCapture("../FaceDetection-project/Video/03.mp4")
-cap = cv2.VideoCapture(1)
-pTime = 0
+    def __init__(self, mode = False, upBody = False,
+                 smooth = True, detectionCon = 0.5, trackCon =0.5):
+        self.mode = mode
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon =trackCon
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.mode, self.upBody, self.smooth,
+                                     self.detectionCon, self.trackCon)
 
-while True:
-    _, img = cap.read()
-    # img = cv2.resize(img, (640, 480))
-    imRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = pose.process(imRGB)
-    print(results.pose_landmarks)
+    def findPose(self, img, draw =True):
+        imRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imRGB)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+        return img
 
-    if results.pose_landmarks:
-        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
-        for id,lm in enumerate(results.pose_landmarks.landmark):
-            h ,w, c =img.shape
+    def findPosition(self, img, draw =True):
+        lmList = []
+        for id,lm in enumerate(self.results.pose_landmarks.landmark):
+            h, w, c = img.shape
             # print(id, lm)
             cx, cy = int(lm.x * w), int(lm.y * h)
-            cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+            lmList.append([id, cx, cy])
+            if draw:
+                cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+        return lmList
 
-    # ---show FPS on image
-    cTime = time.time()
-    fps = 1/(cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, f'FPS: {int(fps)}', (20, 70),
-                cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
+def main():
+    cap = cv2.VideoCapture("../FaceDetection-project/Video/02.mp4")
+    #cap = cv2.VideoCapture(1)
+    pTime = 0
+    detector = poseDetector()
+
+    while True:
+        _, img = cap.read()
+        img = detector.findPose(img)
+        lmList = detector.findPosition(img, draw=False)
+        if len(lmList) != 0:
+            cv2.circle(img, (lmList[12][1], lmList[12][2]), 15, (0, 0, 255), cv2.FILLED)
+            print(lmList[12])
+
+        # ---show FPS on image
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        cv2.putText(img, f'FPS: {int(fps)}', (20, 70),
+                    cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
+
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 
-    cv2.imshow("Image",img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    main()
