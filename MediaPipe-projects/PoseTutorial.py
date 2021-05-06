b65c6278add14e79887cfd477c2490f4b14af4e8
dainioshka <dainioshka@gmail.com>
2021-05-06 20:06:58 +0300

    finish  mediapipe projects dir

--- MediaPipe-projects/PoseTutorial.py
+++ MediaPipe-projects/PoseTutorial.py
@@ -10,6 +10,9 @@ mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
 cap = cv2.VideoCapture(1)
+# Curl counter variables
+counter = 0
+stage = None
 
 def calculate_angle(a, b, c):
     a = np.array(a)
@@ -22,8 +25,6 @@ def calculate_angle(a, b, c):
     if angle > 180.0:
         angle = 360 - angle
     return angle
-
-
 
 
 # Setup mediapipe instance
@@ -43,9 +44,44 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
 
         try:
             landmarks = results.pose_landmarks.landmark
-            print(landmarks)
+            # print(landmarks)
+            # Get cordinates
+            shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+            wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+
+            # Calculate angle
+            angle = calculate_angle(shoulder, elbow, wrist)
+            #print(angle)
+
+            # Visualize angle
+            cv2.putText(image, str(angle),
+                        tuple(np.multiply(elbow, [640, 480]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+            # Curl counter logic
+            if angle > 160:
+                stage = "down"
+            if angle < 30 and stage == 'down':
+                stage = "up"
+                counter += 1
+                print(counter)
         except:
             pass
+        # Render curl counter
+        # Setup status box
+        cv2.rectangle(image, (0, 0), (225, 100), (245, 117, 16), -1)
+
+        # Rep data
+        cv2.putText(image, "REPS", (15, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(counter), (10, 90),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255), 2, cv2.LINE_AA)
+        # Stage data
+        cv2.putText(image, "STAGE", (95, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(image, stage, (95, 90),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255), 2, cv2.LINE_AA)
 
         # Render detection
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
