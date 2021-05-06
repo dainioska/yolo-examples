784ea39dbb36665498d38ed88f48af76a23f2f3a
dainioshka <dainioshka@gmail.com>
2021-05-06 14:02:14 +0300

    add mediapipe projects dir

--- MediaPipe-projects/PoseTutorial.py
+++ MediaPipe-projects/PoseTutorial.py
@@ -11,13 +11,51 @@ mp_pose = mp.solutions.pose
 
 cap = cv2.VideoCapture(1)
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    # frame = cv2.resize(img, (640, 480))
-    cv2.imshow("MPfeed", frame)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
+def calculate_angle(a, b, c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180/np.pi)
+
+    if angle > 180.0:
+        angle = 360 - angle
+    return angle
 
 
-cap.release()
-cv2.destroyAllWindows()
+
+
+# Setup mediapipe instance
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        # frame = cv2.resize(img, (640, 480))
+
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+
+        results = pose.process(image)
+
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        try:
+            landmarks = results.pose_landmarks.landmark
+            print(landmarks)
+        except:
+            pass
+
+        # Render detection
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                  mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2))
+
+        cv2.imshow("MPfeed", image)
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+
+    cap.release()
+    cv2.destroyAllWindows()
